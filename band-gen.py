ffdeb78770a8d46cc412f261c4e16085d310f546
Ahmad Ata Mohammad <37484241+ahmed3ta@users.noreply.github.com>
2022-07-01 05:35:02 +0200

    Delete loader.rb
    Delete kubeadm directory
    
     delete old commit
    
    pyhton print
    
    clean star
    
    day2 is done
    
    one commit on yourBranch

---
+++ band-gen.py
@@ -0,0 +1,6 @@
+#Band Name Creator
+#Author Ahmed Atta Mohamed
+print("This is a Band name Generator")
+city = input("please state your City")
+pet = input("please Enter the name of your pet")
+print("yout Band name can be " + city + pet)
